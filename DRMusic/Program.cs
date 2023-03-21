8b17ec6cfe5470e83b24df51659c7d4ba949cbe9
miksezea <mikk289e@edu.zealand.dk>
2023-03-21 10:18:04 +0100

    CORS rettet

--- DRMusic/Program.cs
+++ DRMusic/Program.cs
@@ -3,7 +3,14 @@ using DRMusic.Repositories;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: "AllowAll",
+                              policy =>
+                              {
+                                  policy.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader();
+                              });
+});
 builder.Services.AddControllers();
 builder.Services.AddSingleton<MusicRecordsRepository>(new MusicRecordsRepository());
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -13,15 +20,16 @@ builder.Services.AddSwaggerGen();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+if (app.Environment.IsDevelopment()) { }
+
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
+
+app.UseCors("AllowAll");
 
 app.MapControllers();
 
