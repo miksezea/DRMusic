60f1554a9a2f4c2edcb01ccc8f7face950606667
miksezea <mikk289e@edu.zealand.dk>
2023-03-21 11:49:48 +0100

    test

--- DRMusic/Program.cs
+++ DRMusic/Program.cs
@@ -13,6 +13,7 @@ builder.Services.AddCors(options =>
 });
 builder.Services.AddControllers();
 builder.Services.AddSingleton<MusicRecordsRepository>(new MusicRecordsRepository());
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -20,8 +21,6 @@ builder.Services.AddSwaggerGen();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment()) { }
-
 app.UseSwagger();
 app.UseSwaggerUI();
 
